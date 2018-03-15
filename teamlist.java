970a7afd18786a439d725c2741662bd57cdcda0a
UltimatePenguin <pmaida1357@gmail.com>
2018-03-15 12:46:26 -0400

    added my name

--- teamlist.java
+++ teamlist.java
@@ -1,7 +1,7 @@
 public class GitExercise{
 	public static void main(String[] args){
 		System.out.println("Eric Chiem");
-		System.out.println("Adam Sargent")
+		System.out.println("Adam Sargent");
 		System.out.println("Peter Maida");
 	}
 }
