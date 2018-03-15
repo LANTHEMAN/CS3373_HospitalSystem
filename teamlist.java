9a7493a88e3d3cf0d5ed0507246baf52d11eeb1f
Alex Tavares <amtavares@wpi.edu>
2018-03-15 14:28:59 -0400

    name added

--- teamlist.java
+++ teamlist.java
@@ -2,7 +2,8 @@ public class teamlist{
 	public static void main(String[] args){
 		System.out.println("Jake Scheide");
 		System.out.println("Eric Chiem");
-		System.out.println("Justin Amevor")
+		System.out.println("Justin Amevor");
 		System.out.println("Adam Sargent");
+		System.out.println("Alex Tavares");
 	}
 }
