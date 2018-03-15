fb70f8d774c933005da81ae537ffb99f87198cbc
Jake Scheide <jcscheide@wpi.edu>
2018-03-15 12:44:46 -0400

    Added my name.

--- teamlist.java
+++ teamlist.java
@@ -2,6 +2,6 @@ public class teamlist{
 	public static void main(String[] args){
 		System.out.println("Jake Scheide");
 		System.out.println("Eric Chiem");
-		System.out.println("Adam Sargent")
+		System.out.println("Adam Sargent");
 	}
 }
