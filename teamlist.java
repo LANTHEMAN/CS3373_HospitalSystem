deb6b192012610663c0eb9051b5a01decdecd0dc
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-03-15 16:12:41 -0400

    Added my name

--- teamlist.java
+++ teamlist.java
@@ -2,9 +2,9 @@ public class teamlist{
 	public static void main(String[] args){
 		System.out.println("Jake Scheide");
 		System.out.println("Eric Chiem");
-		System.out.println("Adam Sargent")
-		System.out.println("Lanhao Mao");
 		System.out.println("Justin Amevor");
+		System.out.println("Adam Sargent");
 		System.out.println("Alex Tavares");
+		System.out.println("Matthew Kaminski");
 	}
 }
