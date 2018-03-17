49cff3cd3dbfb3866dcad95dd9b4d86db387333b
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-03-17 14:18:01 -0400

    Set up gradle project structure

--- src/main/java/com/github/CS3733_D18_Team_F_Project_0/Main.java
+++ src/main/java/com/github/CS3733_D18_Team_F_Project_0/Main.java
@@ -1,4 +1,6 @@
-public class teamlist {
+package com.github.CS3733_D18_Team_F_Project_0;
+
+public class Main {
 	public static void main(String[] args) {
 		System.out.println("Jake Scheide");
 		System.out.println("Eric Chiem");
@@ -9,6 +11,5 @@ public class teamlist {
 		System.out.println("Matthew Kaminski");
 		System.out.println("Lanhao Mao");
 		System.out.println("Peter Maida");
-
 	}
 }
