0b31912b7f48ec5fed7ebf55b2f719b02c805d49
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-03-22 11:27:46 -0400

    Made derby test file
    Includes a dummy test

---
+++ src/test/java/com/github/CS3733_D18_Team_F_Project_0/DerbyTest.java
@@ -0,0 +1,12 @@
+package com.github.CS3733_D18_Team_F_Project_0;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DerbyTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 1);
+    }
+}
