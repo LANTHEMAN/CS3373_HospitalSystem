d3a02d5dbea9f0933b239ef43a9237f40cbbd09e
Unknown <tgdubuke@wpi.edu>
2018-03-22 11:08:46 -0400

    Update Node with 3D point
    Used javafx.geometry.Point3D to create node position

--- src/main/java/com/github/CS3733_D18_Team_F_Project_0/Node.java
+++ src/main/java/com/github/CS3733_D18_Team_F_Project_0/Node.java
@@ -1,5 +1,14 @@
 package com.github.CS3733_D18_Team_F_Project_0;
 
+import java.util.HashSet;
+import javafx.geometry.Point3D;
+
 public class Node {
-    private double x, y, z;
+    private Point3D position;
+    private Hashset<Node> neighbors;
+
+    Node(Point3D position, Hashset<Node> neighbors) {
+        this.position = position;
+        this.neighbors = neighbors;
+    }
 }
