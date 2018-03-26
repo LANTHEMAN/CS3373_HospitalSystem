0805a5b976ddbe9532036baa9710ca9b31be235c
Peter Maida <pmaida1357@gmail.com>
2018-03-26 16:34:11 -0400

    added unbounded zoom

--- src/main/java/com/github/CS3733_D18_Team_F_Project_0/HomeController.java
+++ src/main/java/com/github/CS3733_D18_Team_F_Project_0/HomeController.java
@@ -6,16 +6,28 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.geometry.Rectangle2D;
 
 import java.io.IOException;
 
 public class HomeController implements SwitchableController {
 
     private PaneSwitcher switcher;
+
+    @FXML
+    private ImageView ivMap;
+
+    @FXML
+    private ImageView ivZoomIn;
+
+    @FXML
+    private ImageView ivZoomOut;
 
     @FXML
     public Button DirectionsSwitch;
@@ -54,6 +66,19 @@ public class HomeController implements SwitchableController {
     @FXML
     void onFloorSwitch() {
         switcher.switchTo(Screens.Floor);
+    }
+
+    @FXML
+    void onZoomIn() {
+        // Viewports?
+        ivMap.setFitHeight(ivMap.getFitHeight() * 2);
+        ivMap.setFitWidth(ivMap.getFitWidth() * 2);
+    }
+
+    @FXML
+    void onZoomOut() {
+        ivMap.setFitHeight(ivMap.getFitHeight() * 0.5);
+        ivMap.setFitWidth(ivMap.getFitWidth() * 0.5);
     }
 
 }
