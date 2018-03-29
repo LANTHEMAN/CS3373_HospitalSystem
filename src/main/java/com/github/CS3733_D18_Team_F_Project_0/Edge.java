c17d952934ab1d77e235bdddaec77e86b831acef
Unknown <tgdubuke@wpi.edu>
2018-03-29 19:41:05 -0400

    Creation of edge class

---
+++ src/main/java/com/github/CS3733_D18_Team_F_Project_0/Edge.java
@@ -0,0 +1,13 @@
+package com.github.CS3733_D18_Team_F_Project_0;
+
+public class Edge {
+    protected String edgeID;
+    private Node node1;
+    private Node node2;
+
+    public Edge(Node node1, Node node2, String edgeID){
+        this.edgeID = edgeID;
+        this.node1 = node1;
+        this.node2 = node2;
+    }
+}
