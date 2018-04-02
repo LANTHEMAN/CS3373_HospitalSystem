0d582d721cb27052f9406aee43d1fc407047bc3e
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-04-02 11:25:16 -0400

    Added ability to get root from PaneSwitcher

--- src/main/java/com/github/CS3733_D18_Team_F_Project_0/controller/page/HomeController.java
+++ src/main/java/com/github/CS3733_D18_Team_F_Project_0/controller/page/HomeController.java
@@ -10,11 +10,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
@@ -69,6 +71,11 @@ public class HomeController implements SwitchableController {
     public void initialize(PaneSwitcher switcher) {
         this.switcher = switcher;
         map = MapSingleton.getInstance().getMap();
+
+        /*
+        Pane root = switcher.getPane(Screens.Home);
+        root.getChildren().add(new Button("Hello World"));
+        */
 
         cboxDestinationType.getItems().clear();
         cboxDestinationType.getItems().addAll(
