d1abe861f46514949ed420690fb0b91440d60a24
Peter Maida <pmaida1357@gmail.com>
2018-04-02 21:30:07 -0400

    Removed files relating to old switchFloor
    removed fxml, controller, and instance in fxml loader

--- src/main/java/com/github/CS3733_D18_Team_F_Project_0/controller/Screens.java
+++ src/main/java/com/github/CS3733_D18_Team_F_Project_0/controller/Screens.java
@@ -6,7 +6,6 @@ public class Screens {
 
     public static final Screen Home = new Screen("home.fxml", HomeController.class);
     public static final Screen Login = new Screen("login.fxml", LoginController.class);
-    public static final Screen Floor = new Screen("selectFloor.fxml", FloorController.class);
     public static final Screen ServiceRequest = new Screen("serviceRequest.fxml", ServiceRequestController.class);
     public static final Screen ReligiousServices = new Screen("religiousServices.fxml", ReligiousServicesController.class);
     public static final Screen LanguageInterpreter = new Screen("languageInterpreter.fxml", LanguageInterpreterController.class);
