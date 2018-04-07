6cd1fd488f05b7c03e9dc723a3117079cf0ab9cd
Tyler Dubuke <tgdubuke@wpi.edu>
2018-04-06 22:45:15 -0400

    Beginning of voice rec

--- src/main/java/com/github/CS3733_D18_Team_F_Project_0/Main.java
+++ src/main/java/com/github/CS3733_D18_Team_F_Project_0/Main.java
@@ -2,20 +2,23 @@ package com.github.CS3733_D18_Team_F_Project_0;
 
 import com.github.CS3733_D18_Team_F_Project_0.controller.PaneSwitcher;
 import com.github.CS3733_D18_Team_F_Project_0.controller.Screens;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 
 public class Main extends Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         long fileSize = 0;
         // get rid of the database folder if its empty
         try {
@@ -33,8 +36,28 @@ public class Main extends Application {
             }
         } catch (IOException e) {
         }
-        
-        launch(args);
+
+        Configuration configuration = new Configuration();
+
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+        // Start recognition process pruning previously cached data.
+
+        recognizer.startRecognition(true);
+        SpeechResult result = recognizer.getResult();
+
+        System.out.println("Hello");
+        while(true){
+            result = recognizer.getResult();
+            System.out.println(result.getHypothesis());
+        }
+        // Pause recognition process. It can be resumed then with startRecognition(false).
+        //recognizer.stopRecognition();
+
+        //launch(args);
     }
 
     @Override
