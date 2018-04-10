d531f1518ca4cc51f6ed373de270fb14484d6840
Alex Tavares <amtavares@wpi.edu>
2018-04-10 03:37:39 -0400

    added search service request to home.fxml

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/HomeController.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/controller/page/HomeController.java
@@ -16,6 +16,8 @@ import edu.wpi.cs3733d18.teamF.gfx.impl.UglyMapDrawer;
 import edu.wpi.cs3733d18.teamF.graph.NewNodeBuilder;
 import edu.wpi.cs3733d18.teamF.graph.Node;
 import edu.wpi.cs3733d18.teamF.graph.NodeBuilder;
+import edu.wpi.cs3733d18.teamF.sr.ServiceRequest;
+import edu.wpi.cs3733d18.teamF.sr.ServiceRequestSingleton;
 import edu.wpi.cs3733d18.teamF.voice.VoiceLauncher;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.animation.*;
@@ -26,21 +28,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 import javafx.util.Duration;
 import net.kurobako.gesturefx.GesturePane;
 import org.apache.commons.math3.analysis.function.Floor;
@@ -228,6 +227,60 @@ public class HomeController implements SwitchableController, Observer {
     private JFXHamburger hamburgerD;
     @FXML
     private FontAwesomeIconView cancelMenu;
+    @FXML
+    private AnchorPane searchPane;
+
+    ////////////////////////////////////////////////////
+    //                                                //
+    //           Search Service Request Variables     //
+    //                                                //
+    ////////////////////////////////////////////////////
+    String searchType;
+    String filter;
+    private ObservableList<ServiceRequest> listRequests;
+
+
+    @FXML
+    public ComboBox filterType;
+    @FXML
+    public ComboBox availableTypes;
+
+    @FXML
+    public TableView<ServiceRequest> searchResultTable;
+    @FXML
+    public TableColumn btnsCol;
+    @FXML
+    public TableColumn<ServiceRequest, Integer> idNumberCol;
+    @FXML
+    public TableColumn<ServiceRequest, String> requestTypeCol;
+    @FXML
+    public TableColumn<ServiceRequest, String> firstNameCol;
+    @FXML
+    public TableColumn<ServiceRequest, String> lastNameCol;
+    @FXML
+    public TableColumn<ServiceRequest, String> destinationCol;
+    @FXML
+    public TableColumn<ServiceRequest, Integer> requestPriorityCol;
+    @FXML
+    public TableColumn<ServiceRequest, String> theStatusCol;
+
+    private final ObservableList<String> priority = FXCollections.observableArrayList(
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5");
+    private final ObservableList<String> status = FXCollections.observableArrayList(
+            "Incomplete",
+            "In Progress",
+            "Complete");
+    private final ObservableList<String> type = FXCollections.observableArrayList(
+            "Language Interpreter",
+            "Religious Services");
+
+
+
 
     @Override
     public void initialize(PaneSwitcher switcher) {
@@ -755,6 +808,23 @@ public class HomeController implements SwitchableController, Observer {
         }
     }
 
+    @FXML
+    private void onSearchServiceRequest(){
+        filter = "none";
+        searchType = "none";
+
+        filterType.getItems().addAll("Priority", "Status", "Type");
+
+        String lastSearch = ServiceRequestSingleton.getInstance().getLastSearch();
+        String lastFilter = ServiceRequestSingleton.getInstance().getLastFilter();
+        if(lastSearch != null && lastFilter != null){
+            searchType = lastSearch;
+            filter = lastFilter;
+        }
+        onSearch();
+        searchPane.setVisible(true);
+    }
+
 
     // Add location on map
 
@@ -919,5 +989,168 @@ public class HomeController implements SwitchableController, Observer {
             String cmd = (String) arg;
             commands.add(cmd);
         }
+    }
+
+
+
+    ////////////////////////////////////////////////////
+    //                                                //
+    //           Search Service Request Functions     //
+    //                                                //
+    ////////////////////////////////////////////////////
+
+
+
+    @FXML
+    void onFilterType() {
+        searchType = "none";
+        filter = "none";
+        try {
+            if (filterType.getSelectionModel().getSelectedItem().equals("Priority")) {
+                searchType = "Priority";
+                availableTypes.setItems(priority);
+                availableTypes.setVisible(true);
+            } else if (filterType.getSelectionModel().getSelectedItem().equals("Status")) {
+                searchType = "Status";
+                availableTypes.setItems(status);
+                availableTypes.setVisible(true);
+            } else if (filterType.getSelectionModel().getSelectedItem().equals("Type")) {
+                searchType = "Type";
+                availableTypes.setItems(type);
+                availableTypes.setVisible(true);
+            }
+        } catch (NullPointerException e) {
+            searchType = "none";
+        }
+    }
+
+    @FXML
+    void onAvailableTypes() {
+        try {
+            filter = availableTypes.getSelectionModel().getSelectedItem().toString();
+        } catch (NullPointerException e) {
+            filter = "none";
+        }
+    }
+
+    @FXML
+    void onSearch() {
+        ArrayList<ServiceRequest> requests = new ArrayList<>();
+        try {
+            if (filter.equalsIgnoreCase("none")) {
+                ResultSet all = ServiceRequestSingleton.getInstance().getRequests();
+                requests = ServiceRequestSingleton.getInstance().resultSetToServiceRequest(all);
+                all.close();
+            } else {
+                switch (searchType) {
+                    case "Priority":
+                        ResultSet rp = ServiceRequestSingleton.getInstance().getRequestsOfPriority(Integer.parseInt(filter));
+                        requests = ServiceRequestSingleton.getInstance().resultSetToServiceRequest(rp);
+                        rp.close();
+                        break;
+
+                    case "Status":
+                        ResultSet rs = ServiceRequestSingleton.getInstance().getRequestsOfStatus(filter);
+                        requests = ServiceRequestSingleton.getInstance().resultSetToServiceRequest(rs);
+                        rs.close();
+                        break;
+
+                    case "Type":
+                        ResultSet rt = ServiceRequestSingleton.getInstance().getRequestsOfType(filter);
+                        requests = ServiceRequestSingleton.getInstance().resultSetToServiceRequest(rt);
+                        rt.close();
+                        break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            searchResultTable.getItems().clear();
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+        //TODO: put result of search into table
+        if (requests.size() < 1) {
+            //TODO: indicate to user that there are no results
+            return;
+        } else {
+            listRequests = FXCollections.observableArrayList(requests);
+        }
+
+        searchResultTable.setEditable(false);
+
+        idNumberCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, Integer>("id"));
+        requestTypeCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("type"));
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("firstName"));
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("lastName"));
+        destinationCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String >("location"));
+        requestPriorityCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, Integer>("priority"));
+        theStatusCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("status"));
+        btnsCol.setCellValueFactory(new PropertyValueFactory<>("DUMMY"));
+
+        Callback<TableColumn<ServiceRequest, String>, TableCell<ServiceRequest, String>> cellFactory
+                = //
+                new Callback<TableColumn<ServiceRequest, String>, TableCell<ServiceRequest, String>>() {
+                    @Override
+                    public TableCell call(final TableColumn<ServiceRequest, String> param) {
+                        final TableCell<ServiceRequest, String> cell = new TableCell<ServiceRequest, String>() {
+
+                            JFXButton btn = new JFXButton("Select");
+
+                            @Override
+                            public void updateItem(String item, boolean empty) {
+                                super.updateItem(item, empty);
+                                if (empty) {
+                                    setGraphic(null);
+                                    setText(null);
+                                } else {
+                                    btn.setOnAction(event -> {
+                                        ServiceRequest s = getTableView().getItems().get(getIndex());
+                                        onSelect(s);
+                                    });
+                                    setGraphic(btn);
+                                    setText(null);
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                };
+
+        btnsCol.setCellFactory(cellFactory);
+
+        searchResultTable.setItems(listRequests);
+
+        ServiceRequestSingleton.getInstance().setSearch(filter, searchType);
+    }
+
+    @FXML
+    public void onSelect(ServiceRequest s){
+        ServiceRequestSingleton.getInstance().setPopUpRequest(s);
+        switcher.popup(Screens.ServiceRequestPopUp);
+    }
+
+    @FXML
+    void onClear() {
+        availableTypes.setVisible(false);
+        availableTypes.valueProperty().set(null);
+        filterType.valueProperty().set(null);
+        searchType = "none";
+        filter = "none";
+        try {
+            searchResultTable.getItems().clear();
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+        ServiceRequestSingleton.getInstance().setSearchNull();
+    }
+
+    @FXML
+    void onCancel() {
+        ServiceRequestSingleton.getInstance().setSearchNull();
+        switcher.switchTo(Screens.ServiceRequest);
     }
 }
