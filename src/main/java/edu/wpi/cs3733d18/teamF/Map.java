15d4ef949c348b1cbb673e4a051ac5a4518a2e20
Tyler Dubuke <tgdubuke@wpi.edu>
2018-04-10 16:01:50 -0400

    Voice recognition update

--- src/main/java/edu/wpi/cs3733d18/teamF/Map.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/Map.java
@@ -222,12 +222,14 @@ public class Map extends Observable implements DatabaseItem, Observer {
 
     //Note: This function gets you the closest node on the specified floor. Don't use this if you don't know what floor you're looking for!
     public Node findNodeClosestTo(double x1, double y1, boolean is2D) {
+        return findNodeClosestTo(x1, y1, is2D, node -> node != null);
+    }
+
+    //Note: This function gets you the closest node on the specified floor. Don't use this if you don't know what floor you're looking for!
+    public Node findNodeClosestTo(double x1, double y1, boolean is2D, Predicate<Node> destFilter) {
         double closestDistance = Double.MAX_VALUE;
         Node closestNode = null;
-        for (Node n : getNodes()) {
-            if (!floor.equals(n.getFloor())) {
-                continue;
-            }
+        for (Node n : getNodes(destFilter)) {
             double x2 = n.getPosition().getX();
             double y2 = n.getPosition().getY();
             if(!is2D){
