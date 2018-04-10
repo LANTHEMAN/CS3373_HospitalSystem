ac2c4304a9168a396dddfc101ef40e1c95176013
Jake Scheide <jcscheide@wpi.edu>
2018-04-09 21:39:15 -0400

    first attempt, things are in the Path class

--- src/main/java/edu/wpi/cs3733d18/teamF/graph/Path.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/graph/Path.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733d18.teamF.graph;
 
+import javafx.geometry.Point2D;
+
 import java.util.ArrayList;
 
 public class Path {
@@ -51,5 +53,43 @@ public class Path {
         }
         Path path = (Path) obj;
         return this.nodes.equals(path.nodes);
+    }
+
+    public ArrayList<String> makeTextDirections(){
+        ArrayList<String> directions = new ArrayList<String>();
+
+        for(int nodeIndex = 1; nodeIndex < this.nodes.size()-1;nodeIndex++){
+            Node previousNode = this.getNodes().get(nodeIndex-1);
+            Node currentNode = this.getNodes().get(nodeIndex);
+            Node nextNode = this.getNodes().get(nodeIndex + 1);
+            double angle = getAngle(null, currentNode,nextNode);
+            if(angle < 90 && angle > 20){
+                directions.add("Turn right at node " + nodeIndex);
+            } else if (angle < 340 && angle > 270) {
+                directions.add("Turn left at node " + nodeIndex);
+            }
+        }
+        return directions;
+    }
+
+    public double getAngle(Node previousNode, Node currentNode, Node nextNode){
+        //Case for the first node
+        if(previousNode == null){
+            Node n = new NewNodeBuilder().build();
+            //Make a node with a position creating a right triangle, to find the angle between the points.
+            n.setPosition(new Point2D(currentNode.getPosition().getX(),nextNode.getPosition().getY()));
+            previousNode = n;
+        }
+        double previousToCurrent = previousNode.displacementTo(currentNode);
+        double currentToNext = currentNode.displacementTo(nextNode);
+        double previousToNext = previousNode.displacementTo(nextNode);
+        //Law of cosines: csquared = asquared + bsquared - 2ab Cos(C) rearranged to solve for Cos(C)
+        double cosine = (Math.pow(previousToNext,2) - Math.pow(previousToCurrent,2) - Math.pow(currentToNext,2)) / (-1 * (2 * previousToCurrent * currentToNext));
+
+        return Math.toDegrees(Math.acos(cosine));
+
+
+
+
     }
 }
