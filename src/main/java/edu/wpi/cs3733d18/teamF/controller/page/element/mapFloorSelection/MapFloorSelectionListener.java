2bb161c6b3d75ecbc67b4eabfd3cf2c88d3b961a
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-04-14 18:11:00 -0400

    Removed useless files, fixed map ratio

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/mapFloorSelection/MapFloorSelectionListener.java
+++
@@ -1,5 +0,0 @@
-package edu.wpi.cs3733d18.teamF.controller.page.element.mapFloorSelection;
-
-public interface MapFloorSelectionListener {
-
-}
--- src/main/resources/edu/wpi/cs3733d18/teamF/controller/element/mapFloorSelection/MapFloorSelectionElement.java
+++
@@ -1,4 +0,0 @@
-package edu.wpi.cs3733d18.teamF.controller.element.mapFloorSelection;
-
-public class MapFloorSelectionElement {
-}
