33bcb7195e3ba4c1e0acb1eb8b3015f0eda677ce
Unknown <jdamevor98@gmail.com>
2018-04-16 21:48:34 -0400

    made an about page

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/HomeController.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/controller/page/HomeController.java
@@ -334,7 +334,7 @@ public class HomeController implements SwitchableController, Observer, MapViewLi
         aboutElement.initialize(aboutElementPane);
 
         // TODO turn this line into a button
-        aboutElement.hideElement();
+        // aboutElement.hideElement();
 
         // init voice overlay
         paneVoiceController = new PaneVoiceController(voicePane);
@@ -527,7 +527,6 @@ public class HomeController implements SwitchableController, Observer, MapViewLi
         );
         clock.setCycleCount(Animation.INDEFINITE);
         clock.play();
-
     }
 
     @Override
