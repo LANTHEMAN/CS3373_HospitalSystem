58c820dce84093c37e0a8d75a990a85d3f98346c
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-04-23 00:35:40 -0400

    Made labels a lot cleaner, fixed initmap with the ground floor

--- src/main/java/edu/wpi/cs3733d18/teamF/gfx/impl/node/EndNodeDrawer.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/gfx/impl/node/EndNodeDrawer.java
@@ -7,6 +7,7 @@ import edu.wpi.cs3733d18.teamF.gfx.NodeDrawable;
 import edu.wpi.cs3733d18.teamF.graph.Node;
 import javafx.animation.TranslateTransition;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -57,17 +58,15 @@ public class EndNodeDrawer extends NodeDrawable {
         dropEnd.play();
 
         if (!node.getNodeType().equals("HALL")) {
-            JFXTextField endText = new JFXTextField();
+            Label endText = new Label();
 
             endText.setPrefWidth(-1);
             endText.setPrefHeight(-1);
 
-            endText.setTranslateX((posX * pane.getMaxWidth() / imageWidth));
-            endText.setTranslateY((posY * pane.getMaxHeight() / imageHeight) - 10);
+            endText.setTranslateX((posX * pane.getMaxWidth() / imageWidth) - node.getShortName().length() / 2);
+            endText.setTranslateY((posY * pane.getMaxHeight() / imageHeight) - 15);
 
             endText.setText(node.getShortName());
-            endText.setFocusColor(Color.WHITE);
-            endText.setPrefColumnCount((node.getShortName().length() / 4) * 3);
             endText.setAlignment(Pos.CENTER);
             endText.setStyle("-fx-background-color: red; " +
                     "-fx-border-color: black; " +
