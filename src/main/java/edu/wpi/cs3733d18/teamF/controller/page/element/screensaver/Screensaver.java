17f620b4cbcbefb3d04edbf8c5850d9f0d7e3110
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-04-23 00:15:01 -0400

    Display screensaver when idle for duration

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/screensaver/Screensaver.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/screensaver/Screensaver.java
@@ -1,26 +1,39 @@
 package edu.wpi.cs3733d18.teamF.controller.page.element.screensaver;
 
 import edu.wpi.cs3733d18.teamF.controller.page.PageElement;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class Screensaver extends PageElement{
 
     @FXML
     AnchorPane root;
 
+    IdleMonitor idleMonitor;
+
     public void initialize(AnchorPane sourcePane) {
         initElement(sourcePane, root);
+        sourcePane.setOnMouseClicked(this::wakeUp);
+        sourcePane.setOnMouseMoved(this::wakeUp);
+
+        idleMonitor = new IdleMonitor(new Duration(30000), this::start);
+        idleMonitor.startMonitoring();
     }
 
     public void start(){
         showElement();
         root.setStyle("-fx-background-color: rgba(0, 0, 0, 0.5);");
-
     }
 
-    public void stop(){
+    public void wakeUp(Event e){
         hideElement();
+    }
+
+    @Override
+    public void finalize() {
+        idleMonitor.stopMonitoring();
     }
 
 }
