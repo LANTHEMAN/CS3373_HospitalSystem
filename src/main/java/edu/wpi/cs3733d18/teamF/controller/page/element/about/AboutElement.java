66c99ae9fc24c414bcf0b75cc1fd3f14bdc99e3c
Unknown <jdamevor98@gmail.com>
2018-04-23 13:40:30 -0400

    less broke credits

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/about/AboutElement.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/about/AboutElement.java
@@ -1,10 +1,10 @@
 package edu.wpi.cs3733d18.teamF.controller.page.element.about;
 
-        import com.jfoenix.controls.JFXButton;
-        import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
-        import edu.wpi.cs3733d18.teamF.controller.page.PageElement;
-        import javafx.fxml.FXML;
-        import javafx.scene.layout.AnchorPane;
+import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import edu.wpi.cs3733d18.teamF.controller.page.PageElement;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class AboutElement extends PageElement {
     @FXML
@@ -29,6 +29,8 @@ public class AboutElement extends PageElement {
         closeCredits.setOnMouseClicked(e-> hideElement());
     }
 
-
-
+    public void showElement() {
+        super.showElement();
+        creditsPage.setVisible(false);
+    }
 }
