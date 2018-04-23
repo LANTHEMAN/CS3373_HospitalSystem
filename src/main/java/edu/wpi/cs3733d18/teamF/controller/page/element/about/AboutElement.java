98b1a06a880ac7a5bd7929fa60f7ee2163d6e85f
Unknown <jdamevor98@gmail.com>
2018-04-23 11:13:37 -0400

    broken credits

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/about/AboutElement.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/about/AboutElement.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733d18.teamF.controller.page.element.about;
 
+        import com.jfoenix.controls.JFXButton;
         import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
         import edu.wpi.cs3733d18.teamF.controller.page.PageElement;
         import javafx.fxml.FXML;
@@ -12,9 +13,22 @@ public class AboutElement extends PageElement {
     @FXML
     FontAwesomeIconView closeButton;
 
+    @FXML
+    JFXButton creditsButton;
+
+    @FXML
+    FontAwesomeIconView closeCredits;
+
+    @FXML
+    AnchorPane creditsPage;
+
     public void initialize(AnchorPane sourcePane) {
         initElement(sourcePane, root);
         closeButton.setOnMouseClicked(e -> hideElement());
+        creditsButton.setOnMouseClicked(e -> creditsPage.setVisible(true));
+        closeCredits.setOnMouseClicked(e-> hideElement());
     }
+
+
 
 }
