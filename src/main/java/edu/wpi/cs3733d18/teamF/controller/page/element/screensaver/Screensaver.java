373bf3a99d08c9487250788fedf60f9522b480e9
Unknown <jdamevor98@gmail.com>
2018-04-24 12:24:05 -0400

    random akwaba screensaver

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/screensaver/Screensaver.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/screensaver/Screensaver.java
@@ -1,36 +1,65 @@
 package edu.wpi.cs3733d18.teamF.controller.page.element.screensaver;
 
+import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733d18.teamF.controller.page.PageElement;
 import edu.wpi.cs3733d18.teamF.controller.page.element.mapView.MapMementoSingleton;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
+
+import static java.lang.StrictMath.abs;
 
 
 public class Screensaver extends PageElement{
 
+    private  final ArrayList<String> welcome = new ArrayList<>(Arrays.asList("Welcome!"
+    ,"Bienvenue!"
+    ,"benvinguda!"
+    ,"Bienvenido!"
+    ,"Herzlich Willkommen!"
+    ,"welkom!"));
+
+
+
     private final ArrayList<String> askMe = new ArrayList<>(Arrays.asList("Ask me to find the nearest bathroom"
-            , "Ask me to get religous services"
             , "Ask me to find the nearest elevator"
-            , "Ask me to Find the Nearest Stairway"
+            , "Ask me to find the earest stairway"
             , "Ask me to find the parking garage"
-            , "Ask me to bring you to Paitent Services"
-            , "Ask me to get you food"
-            , "Ask me to bring you to"
+            , "Ask me to bring you to paitent services"
+            , "Ask me to do a food request"
+            , "Ask me to bring you to look for places nearby"
+            , "Ask to call for help in an emergency"
+            , "Ask me to play where's Packman"
+            , "Ask me to find the closet elevator"
             , "Ask me to bring you to the nearest ATM"
+            , "Say Hello Kiosk if you want to use voice commands"
+            , "Ask me to go to the tower"
+            , "Ask me to go to the dialysis"
             , "Ask me to bring you to the Shapiro Cardiovascular Building"
             , "Ask me to bring you to the international paitent Center"
-            , "Ask me to bring you to Spiritual Care Office"));
-    @FXML
+            , "Ask me to navigate  to Spiritual Care Office"));
+        @FXML
     AnchorPane root;
     IdleMonitor idleMonitor;
     Scene scene;
+
+    @FXML
+    JFXTextField askMeField;
+
+    @FXML
+    JFXTextField welcomeField;
+
+    Random rand = new Random();
+
 
     public void initialize(AnchorPane sourcePane, Scene scene) {
         initElement(sourcePane, root);
@@ -55,6 +84,18 @@ public class Screensaver extends PageElement{
     public void start(){
         MapMementoSingleton.getInstance().saveState();
         showElement();
+        welcomeField.setText(welcome.get(abs(rand.nextInt()) % welcome.size()));
+        askMeField.setText(askMe.get(abs(rand.nextInt()) % askMe.size()));
+
+        Timeline clock1 = new Timeline(new KeyFrame(Duration.seconds(rand.nextInt()), e ->
+                welcomeField.setText(welcome.get(abs(rand.nextInt()) % askMe.size()))));
+        clock1.setCycleCount(Animation.INDEFINITE);
+        clock1.play();
+
+        Timeline clock = new Timeline(new KeyFrame(Duration.seconds(5), e ->
+                askMeField.setText(askMe.get(abs(rand.nextInt()) % askMe.size()))));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
     }
 
     public void wakeUp(Event e) {
