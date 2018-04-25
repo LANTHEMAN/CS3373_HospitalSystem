423b5ad6e0319980113820d73201cb9dd7bad5e5
Tyler Dubuke <tgdubuke@wpi.edu>
2018-04-25 00:19:33 -0400

    Finished screensaver

--- src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/screensaver/Screensaver.java
+++ src/main/java/edu/wpi/cs3733d18/teamF/controller/page/element/screensaver/Screensaver.java
@@ -1,9 +1,9 @@
 package edu.wpi.cs3733d18.teamF.controller.page.element.screensaver;
 
-import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733d18.teamF.controller.page.PageElement;
 import edu.wpi.cs3733d18.teamF.controller.page.element.mapView.MapMementoSingleton;
 import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.Event;
@@ -11,42 +11,48 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.util.Duration;
 
+import javax.swing.text.html.ImageView;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Random;
 
 import static java.lang.StrictMath.abs;
 
 
-public class Screensaver extends PageElement{
+public class Screensaver extends PageElement {
 
-    private  final ArrayList<String> welcome = new ArrayList<>(Arrays.asList("WELCOME!"
-    ,"BIENVENUE!"
-    ,"BENVINGUDA!"
-    ,"BIENVENIDO!"
-    ,"HERZLICH WILLKOMMEN!"
-    ,"WELKOM!"));
+    private final ArrayList<String> welcome = new ArrayList<>(Arrays.asList(
+            "WELCOME!"
+            , "BIENVENUE!"
+            , "BENVINGUDA!"
+            , "BIENVENIDO!"
+            , "HERZLICH WILLKOMMEN!"
+            , "WELKOM!"
+            , "GHINI VINISHI!"));
 
-    private final ArrayList<String> askMe = new ArrayList<>(Arrays.asList("Ask me to find the nearest bathroom"
-            , "Ask me to find the nearest elevator"
-            , "Ask me to find the earest stairway"
+    private final ArrayList<String> askMe = new ArrayList<>(Arrays.asList(
+            "Ask me to find the nearest bathroom"
+            , "Ask me the Weather"
+            , "Ask me where the dentist is"
             , "Ask me to find the parking garage"
-            , "Ask me to bring you to paitent services"
-            , "Ask me to do a food request"
-            , "Ask me to bring you to look for places nearby"
-            , "Ask to call for help in an emergency"
-            , "Ask me to play where's Pacman"
-            , "Ask me to find the closet elevator"
-            , "Ask me to bring you to the nearest ATM"
-            , "Say Hello Kiosk if you want to use voice commands"
-            , "Ask me to go to the tower"
-            , "Ask me to go to the dialysis"
-            , "Ask me to bring you to the Shapiro Cardiovascular Building"
-            , "Ask me to bring you to the international paitent Center"
-            , "Ask me to navigate  to Spiritual Care Office"));
+            , "Ask me where the Cafe is located"
+            , "Ask me to find the Ear Nose and Throat Doctor"
+            , "Ask me to find the closest exit"
+            , "Ask me to find the closest elevator"
+            , "Say Hello Kiosk to use voice commands"
+            , "Ask me to Neuroscience"
+            , "Ask me to Rap"
+            , "Ask me to go to Orthopedics and Rhemutology"
+            , "Ask me to go to Plastic Surgery"
+            , "Ask directions to Radiology"
+            , "Ask me directions to Nuclear Medicine"
+            , "Ask me where the Duncan Reid Conference Room is"
+            , "Ask me how to get to the Lee Bell Breast Center"
+            , "Click anywhere on the map to get directions"
+            , "Play a game of Where's Pacman if you are bored"));
     @FXML
     AnchorPane root;
 
@@ -57,9 +63,65 @@ public class Screensaver extends PageElement{
     Label askMeField;
 
     @FXML
+    Label time;
+
+    @FXML
+    Label date;
+
+    @FXML
     Label welcomeField;
 
+    @FXML
+    ImageView Background;
+
     Random rand = new Random();
+
+
+    Timeline welcomeTime = new Timeline(new KeyFrame(Duration.seconds(5), e -> {
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5), welcomeField);
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+        fadeOut.play();
+
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.5), welcomeField);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+
+        fadeOut.setOnFinished(event -> {
+            welcomeField.setText(welcome.get(abs(rand.nextInt()) % welcome.size()));
+            fadeIn.play();
+        });
+    }));
+
+    Timeline askMeTime = new Timeline(new KeyFrame(Duration.seconds(4), e -> {
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5), askMeField);
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+        fadeOut.play();
+
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.5), askMeField);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+
+        fadeOut.setOnFinished(event -> {
+            askMeField.setText(askMe.get(abs(rand.nextInt()) % askMe.size()));
+            fadeIn.play();
+        });
+    }));
+
+    private String start;
+
+    Timeline clock = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
+        Calendar cal = Calendar.getInstance();
+        int second = cal.get(Calendar.SECOND);
+        int minute = cal.get(Calendar.MINUTE);
+        int hour = cal.get(Calendar.HOUR) % 12 + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int month = cal.get(Calendar.MONTH) % 12 + 1;
+        int year = cal.get(Calendar.YEAR);
+        time.setText(hour + ":" + String.format("%02d", minute) + ":" + String.format("%02d", second));
+        date.setText(month + "/" + day + "/" + year);
+    }));
 
 
     public void initialize(AnchorPane sourcePane, Scene scene) {
@@ -69,12 +131,25 @@ public class Screensaver extends PageElement{
 
         this.scene = scene;
 
-        idleMonitor = new IdleMonitor(new Duration(1000), this::start);
+        idleMonitor = new IdleMonitor(new Duration(30000), this::start);
         idleMonitor.startMonitoring();
         idleMonitor.register(scene, Event.ANY);
+
+        System.out.println("Init");
+
+        welcomeField.setText(welcome.get(abs(rand.nextInt()) % welcome.size()));
+        askMeField.setText(askMe.get(abs(rand.nextInt()) % askMe.size()));
+
+        welcomeTime.setCycleCount(Animation.INDEFINITE);
+        welcomeTime.play();
+        askMeTime.setCycleCount(Animation.INDEFINITE);
+        askMeTime.play();
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+
     }
 
-    public void changeTimeout(int millis){
+    public void changeTimeout(int millis) {
         finalize();
 
         idleMonitor = new IdleMonitor(new Duration(millis), this::start);
@@ -82,21 +157,9 @@ public class Screensaver extends PageElement{
         idleMonitor.register(scene, Event.ANY);
     }
 
-    public void start(){
+    public void start() {
         MapMementoSingleton.getInstance().saveState();
         showElement();
-        welcomeField.setText(welcome.get(abs(rand.nextInt()) % welcome.size()));
-        askMeField.setText(askMe.get(abs(rand.nextInt()) % askMe.size()));
-
-        Timeline clock1 = new Timeline(new KeyFrame(Duration.seconds((abs(rand.nextInt()) % 5 + 10)), e ->
-                welcomeField.setText(welcome.get(abs(rand.nextInt()) % welcome.size()))));
-        clock1.setCycleCount(Animation.INDEFINITE);
-        clock1.play();
-
-        Timeline clock = new Timeline(new KeyFrame(Duration.seconds(5), e ->
-                askMeField.setText(askMe.get(abs(rand.nextInt()) % askMe.size()))));
-        clock.setCycleCount(Animation.INDEFINITE);
-        clock.play();
     }
 
     public void wakeUp(Event e) {
